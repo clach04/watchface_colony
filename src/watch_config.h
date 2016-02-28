9b6c9077fec8b6a130802fb52ee0ca8b47c1c3d6
clach04 <Chris.Clark@actian.com>
2016-02-28 17:28:40 +0100

    initial version
    needs lots of tweaking. Basalt ONLY!

--- src/watch_config.h
+++ src/watch_config.h
@@ -1,5 +1,11 @@
 #pragma once
 
+#define BG_IMAGE RESOURCE_ID_COLONY_LOGO
+#define IMAGE_RES_X 132  /* NOTE if image res changes - this needs to change too! */
+#define IMAGE_RES_Y 132  /* NOTE if image res changes - this needs to change too! */
+
+//DEFAULT_BACKGROUND_COLOR GColorRed
+
 #define USE_GENERIC_MAIN
 #undef REMOVE_LEADING_ZERO_FROM_TIME  /* 12 hour display will not have a leading "0" if this is defined */
 #undef FONT_NAME
@@ -8,37 +14,35 @@
 
 #if defined(PBL_HEALTH)
     #define USE_HEALTH
-    //#define UPDATE_HEALTH_ON_ACTIVITY  /* If not set, only updates step count display once per minute */
+    #undef USE_HEALTH
+    #define HEALTH_POS GRect(0, 40, 144, 168)  // RECT
+    //#define UPDATE_HEALTH_ON_ACTIVITY  // If not set, only updates step count display once per minute
 #endif /* PBL_HEALTH */
 
+#define DATE_FMT_STR "%a\n%b\n%d"  /* TODO review %d for day */
+#define MAX_DATE_STR "Thu\n00\nAug"  /* if custom version of DATE_FMT_STR is set, MAX_DATE_STR  needs to be updated too */
+
 #define DRAW_BATTERY
-#define DRAW_SMALL_BATTERY
+//#define DRAW_SMALL_BATTERY
 
 #ifdef PBL_ROUND /* 180x180 */
-/*TODO center/move right*/
-    #define CLOCK_POS GRect(0, 52, 180, 180) /* probably taller than really needed */
-    #define HEALTH_POS GRect(0, 40, 180, 180)
-    #define BT_POS GRect(0, 100, 180, 180) /* probably taller than really needed */
-
-    #define DATE_ALIGN GTextAlignmentCenter
-    #define DATE_POS GRect(0, 120, 180, 180) /* probably taller than really needed */
-
-    #define BAT_ALIGN GTextAlignmentCenter
-    #ifdef DRAW_BATTERY
-        #define BAT_POS GRect(85, 10, 180, 180) /* probably taller than really needed */
-    #else
-        #define BAT_POS GRect(0, 140, 180, 180) /* probably taller than really needed */
-    #endif /* DRAW_BATTERY */
-
+    /* TODO */
 #else /* PBL_RECT 144x168*/
-    #define CLOCK_POS GRect(0, 52, 144, 168) /* probably taller than really needed */
-    #define HEALTH_POS GRect(0, 40, 144, 168)
+    /* Image at top of screen, centered horizontally */
+    #define BG_IMAGE_GRECT GRect((144 - IMAGE_RES_X) / 2, 0,  IMAGE_RES_X, IMAGE_RES_Y)  // use same size as image. On Aplite can determine this at runtime but not Basalt
+
+    //#define CLOCK_POS GRect(0, 136, 144, 168) /* bottom of screen for FONT_KEY_BITHAM_30_BLACK */
+    #define CLOCK_POS GRect(0, 118, 144, 168) /* bottom of screen for FONT_KEY_ROBOTO_BOLD_SUBSET_49 with one pixel border on bottom */
+
+    //#define BT_DISCONNECT_IMAGE_GRECT GRect(144 - 20, 138, 20, 30)
+    #define BT_DISCONNECT_IMAGE_GRECT GRect(144 - 20, 168 - (2 * 30 + 4), 20, 30)
+
     #define BT_POS GRect(0, 120, 144, 168) /* probably taller than really needed */
-    #define DATE_POS GRect(0, 140, 144, 168) /* probably taller than really needed */
+    #define DATE_POS GRect(0, 0, 144, 168) /* probably taller than really needed */
     #ifdef DRAW_BATTERY
-        #define BAT_POS GRect(5, 150, 144, 168)
+        #define BAT_POS GRect(5, 5, 144, 168)
     #else
-        #define BAT_POS GRect(0, 140, 144, 168) /* probably taller than really needed */
+        #define BAT_POS GRect(0, 0, 144, 168) /* probably taller than really needed */
     #endif /* DRAW_BATTERY */
 #endif /* end of Round or rectangle */
 
